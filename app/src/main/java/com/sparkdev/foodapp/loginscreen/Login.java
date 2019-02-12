08a3bd622d15f2d448d920ecf2ba2ea972815ef6
Dawsonaraujo13 <42874477+dawsonaraujo13@users.noreply.github.com>
2019-02-12 14:37:05 -0500

    Added design features to username and password boxes

--- app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
+++ app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
@@ -1,4 +1,13 @@
 package com.sparkdev.foodapp.loginscreen;
 
-public class Login {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
