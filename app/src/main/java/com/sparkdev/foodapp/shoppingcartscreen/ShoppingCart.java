c7516235578c404e55328c49cc71ad88f3b4f248
David Cruz <dcruz083@fiu.edu>
2019-02-19 14:19:53 -0500

    new shopping cart files

---
+++ app/src/main/java/com/sparkdev/foodapp/shoppingcartscreen/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.sparkdev.foodapp.shoppingcartscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoppingCart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
