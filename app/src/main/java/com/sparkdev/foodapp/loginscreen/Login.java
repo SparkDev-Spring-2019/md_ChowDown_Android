57cf3eeb1287e2c32b7c8b9d369e3ed34d429af2
Dawsonaraujo13 <42874477+dawsonaraujo13@users.noreply.github.com>
2019-02-19 13:36:17 -0500

    working on backend

--- app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
+++ app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
@@ -2,12 +2,38 @@ package com.sparkdev.foodapp.loginscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private static EditText username;
+    private static EditText password;
+    private static Button login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton();
+    }
+
+    public void loginButton(){
+
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+
+        login.setOnClickListener(
+                new View.OnClickListener(){
+
+                    public void onClick(View v)
+
+
+                }
+        );
     }
 }
