1ac4fd8146f4c223471f26b037d5241d993d5f66
David Cruz <dcruz083@fiu.edu>
2019-02-19 14:29:38 -0500

    new order of files

--- app/src/main/java/com/sparkdev/foodapp/shoppingcartscreen/OrderScreen/OrderScreenActivity.java
+++ app/src/main/java/com/sparkdev/foodapp/shoppingcartscreen/OrderScreen/OrderScreenActivity.java
@@ -1,13 +1,13 @@
-package com.sparkdev.foodapp.shoppingcartscreen;
+package com.sparkdev.foodapp.shoppingcartscreen.OrderScreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ShoppingCart extends AppCompatActivity {
+public class OrderScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_cart);
+        setContentView(R.layout.activity_order_screen);
     }
 }
