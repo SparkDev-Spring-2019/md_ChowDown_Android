5919ce826be4b6b498f56e0e9e869959f0258522
Dawsonaraujo13 <42874477+dawsonaraujo13@users.noreply.github.com>
2019-02-21 16:43:43 -0500

    added user model

--- app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
+++ app/src/main/java/com/sparkdev/foodapp/loginscreen/Login.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.sparkdev.foodapp.R;
 
 public class Login extends AppCompatActivity {
 
     private static EditText username;
     private static EditText password;
     private static Button login;
+    private static TextView register;
 
 
 
@@ -26,14 +32,35 @@ public class Login extends AppCompatActivity {
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
         login = (Button)findViewById(R.id.login);
+        register =  (TextView) findViewById(R.id.register);
 
         login.setOnClickListener(
-                new View.OnClickListener(){
+               new View.OnClickListener(){
+                    public void onClick(View v){
+                        if(username.getText().toString().equals("user") && password.getText().equals("pass")){
 
-                    public void onClick(View v)
+                        }
+                        if (username.getText().toString().equals("")) {
+                            Toast.makeText(getApplicationContext(), "Enter a username!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (password.getText().toString().equals("")) {
+                            Toast.makeText(getApplicationContext(), "Enter a password!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (password.length() <= 6) {
+                            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
 
 
-                }
+
+               }
         );
+
+        
     }
 }
