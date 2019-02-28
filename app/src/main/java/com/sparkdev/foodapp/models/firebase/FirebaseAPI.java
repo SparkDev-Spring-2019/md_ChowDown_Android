2f24593279891ae9779cd0d2d94fda95d61ae0b4
czuria1 <czuria1@gmail.com>
2019-02-28 15:30:39 -0500

    Update FirebaseAPI

--- app/src/main/java/com/sparkdev/foodapp/models/firebase/FirebaseAPI.java
+++ app/src/main/java/com/sparkdev/foodapp/models/firebase/FirebaseAPI.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -41,8 +43,28 @@ public class FirebaseAPI {
     }
   }
 
-  public void registerUser(final String email, String password, String firstName, String lastName
-      , final SignUpCompletionListener listener) {
+  public void registerUser(final String email, String password, final SignUpCompletionListener listener) {
+
+    // create an instance of the FirebaseAuth class
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    // call the createUserWithEmailAndPassword from the FirebaseAuth class and add a
+    // listener to execute the argument after the user is registered in the Firebase Authentication
+    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+        new OnCompleteListener<AuthResult>() {
+      @Override
+      public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+          // the user was successfully created in the Firebase Authentication area, BUT NOT in the
+          // Firestore database so get the created user's random generated ID
+          String userId = task.getResult().getUser().getUid();
+
+
+
+        } else {
+
+        }
+      }
+    });
     
   }
 
