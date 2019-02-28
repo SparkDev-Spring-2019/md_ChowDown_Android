e922d6a373eeb510d140010c79cf2be9a7787f4c
czuria1 <czuria1@gmail.com>
2019-02-27 23:51:48 -0500

    Change FirebaseDatabase to FirebaseFirestore dependency

--- app/src/main/java/com/sparkdev/foodapp/models/firebase/FirebaseAPI.java
+++ app/src/main/java/com/sparkdev/foodapp/models/firebase/FirebaseAPI.java
@@ -1,10 +1,16 @@
 package com.sparkdev.foodapp.models.firebase;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.sparkdev.foodapp.models.firebase.foodMenuInterface.GetMenuCompletionListener;
 import com.sparkdev.foodapp.models.firebase.signupInterface.SignUpCompletionListener;
 
 public class FirebaseAPI {
@@ -15,14 +21,14 @@ public class FirebaseAPI {
   // constant to use as a tag for console log messages
   private static final String TAG = "FIREBASE_API";
 
-  private FirebaseDatabase mDatabase;
+  private FirebaseFirestore mFirestore;
 
   private FirebaseAPI(Context context) {
     // initialize the default FirebaseApp instance
     FirebaseApp.initializeApp(context);
     Log.d(TAG, "Firebase Database has been initialized");
     // get the default FirebaseDatabase instance
-    mDatabase = FirebaseDatabase.getInstance();
+    mFirestore = FirebaseFirestore.getInstance();
   }
 
   public static FirebaseAPI getInstance(Context context) {
@@ -38,5 +44,21 @@ public class FirebaseAPI {
   public void registerUser(final String email, String password, String firstName, String lastName
       , final SignUpCompletionListener listener) {
     
+  }
+
+  public void getMenu(final GetMenuCompletionListener listener) {
+
+    CollectionReference menuRef = mFirestore.collection("Menu");
+
+    menuRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+      @Override
+      public void onComplete(@NonNull Task<QuerySnapshot> task) {
+        if (task.isSuccessful()) {
+
+        } else {
+
+        }
+      }
+    });
   }
 }
