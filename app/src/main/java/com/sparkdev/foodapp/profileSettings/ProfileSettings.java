26683e641916f888ef77d7a1d61097a4c19e4bbb
msblaise <42875058+msblaise@users.noreply.github.com>
2019-03-05 13:31:48 -0500

    Added recycler view (profileSettings and adapter class)

--- app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettings.java
+++ app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettings.java
@@ -2,16 +2,43 @@ package com.sparkdev.foodapp.profileSettings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.sparkdev.foodapp.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+This class represents the ContactListActivity which will display all the contacts from an ArrayList.
+ */
+
 public class ProfileSettings extends AppCompatActivity {
 
+    private RecyclerView profileRecyclerView;
+    private ProfileSettingsAdapter profileAdapter;
+    private ArrayList<String> text_rows = new ArrayList<>();
+
+    // The ContactListActivity enters the created state when the activity is created for the first
+    // time (i.e. when the user opens the application).
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
-        //instance for recycler view, adapter
-        //dummy arraylist
+
+        // Add contacts to the contacts ArrayList
+        for(int i = 0; i < 30; i++){
+            text_rows.add("Contact " + i);
+        }
+
+        // Get access to the RecyclerView
+        profileRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        // Create the adapter and supply the adapter with the data (i.e from an arraylist or database)
+        profileAdapter = new ProfileSettingsAdapter(this,text_rows);
+        // Connect the adapter to the RecyclerView
+        profileRecyclerView.setAdapter(profileAdapter);
+        // Define the RecyclerView's default layout manager
+        profileRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettingsAdapter.java
+++ app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettingsAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,6 +28,7 @@ public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettings
     public class ProfileViewHolder extends RecyclerView.ViewHolder{
         // The following variables are for the text view and the adapter for each row
         public final TextView nameTextView;
+        public final EditText editTextView;
         final ProfileSettingsAdapter rowAdapter;
 
         // Constructor where the first parameter is to inflate the layout and the second
@@ -35,7 +37,8 @@ public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettings
             super(itemView);
             // Initialize the view holder's text view from the XML resources (activity_contact_list.xml)
             // Be sure to cast it to the View type that you need it to be (i.e TextView)
-            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            nameTextView = (TextView) itemView.findViewById(R.id.row_text);
+            editTextView = (EditText) itemView.findViewById(R.id.row_edit);
             // Set up the adapter
             this.rowAdapter = adapter;
         }
@@ -48,7 +51,7 @@ public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettings
     @Override
     public ProfileViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         // Inflate the layout
-        View customView = profileInflater.inflate(R.layout.single_contact_layout, viewGroup, false);
+        View customView = profileInflater.inflate(R.layout.profile_edit_text_row, viewGroup, false);
         // Return the new view holder
         return new ProfileViewHolder(customView, this);
     }
