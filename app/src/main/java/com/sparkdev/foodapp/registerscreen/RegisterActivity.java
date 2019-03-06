abd335a620200f98563e78bb1bb9be478e43d6b1
vrive <vanessariveros25@gmail.com>
2019-03-05 21:37:13 -0500

    Add no action bar style

--- app/src/main/java/com/sparkdev/foodapp/registerscreen/RegisterActivity.java
+++ app/src/main/java/com/sparkdev/foodapp/registerscreen/RegisterActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.text.TextUtils;
 import android.widget.Toast;
+import com.sparkdev.foodapp.R;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
