fd3be82cda950ea9c35d85f923f74c3ac793fdde
Javiolau <reivaj.e99@gmail.com>
2019-03-21 17:01:27 -0400

    almost done

--- app/src/main/java/com/sparkdev/foodapp/mainscreen/FoodMenu/FoodMenuActivity.java
+++ app/src/main/java/com/sparkdev/foodapp/mainscreen/FoodMenu/FoodMenuActivity.java
@@ -17,6 +17,13 @@ public class FoodMenuActivity extends AppCompatActivity {
     private FoodMenuAdapter foodMenuAdapter;
     private ArrayList<String> foodItem = new ArrayList<>();
     private ArrayList<Integer> images = new ArrayList<>();
+    private ArrayList<String> prices = new ArrayList<>();
+    private ArrayList<String> categories = new ArrayList<>();
+    private ArrayList<Double> ratings = new ArrayList<Double>();
+    int clockIcon = R.drawable.clock_icon;
+    int starIcon = R.drawable.star_icon;
+    int leafIcon = R.drawable.leaf_icon;
+
 
 
     // The FoodMenuActivity enters the created state when the activity is created for the first
@@ -27,10 +34,24 @@ public class FoodMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_list);
 
         // Filling foodItem ArrayList
-        String [] categoryArray = {"Rice and Beans", "Tiramisu", "Pizza","Cake", "Arroz con leche"};
+        String [] foodNameArray = {"Rice and Beans", "Tiramisu", "Pizza","Cake", "Arroz con leche"};
+        for(int i = 0; i < foodNameArray.length; i++)
+        {
+            foodItem.add(foodNameArray[i]);
+        }
+
+        //Filling categories ArrayList
+        String [] categoryArray = {"Category", "Category", "Category","Category","Category"};
         for(int i = 0; i < categoryArray.length; i++)
         {
-            foodItem.add(categoryArray[i]);
+            categories.add(categoryArray[i]);
+        }
+
+        //Filling ratings ArrayList
+        double [] rateArray = {4.3, 4.8, 1.4, 3.6, 5.0};
+        for(int i = 0; i < rateArray.length; i++)
+        {
+            ratings.add(rateArray[i]);
         }
 
         // Filling images ArrayList
@@ -41,15 +62,24 @@ public class FoodMenuActivity extends AppCompatActivity {
             images.add(imageArray[i]);
         }
 
+        //Fill prices ArrayList
+        String [] pricesArray = {"$4.55", "$8.65", "$2.34", "$3.87", "$7.45"};
+        for(int i = 0; i < pricesArray.length; i++)
+        {
+            prices.add(pricesArray[i]);
+        }
+
 
 
         // Get access to the RecyclerView
         foodMenuRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // Create the adapter and supply the adapter with the data (i.e from an arraylist or database)
-        foodMenuAdapter = new FoodMenuAdapter(this,foodItem, images);
+        foodMenuAdapter = new FoodMenuAdapter(this,foodItem, images, prices, categories , ratings,clockIcon, starIcon, leafIcon);
         // Connect the adapter to the RecyclerView
         foodMenuRecyclerView.setAdapter(foodMenuAdapter);
         // Define the RecyclerView's default layout manager
         foodMenuRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
+
+//, prices, categories , ratings,clockIcon, starIcon, leafIcon
--- app/src/main/java/com/sparkdev/foodapp/mainscreen/FoodMenu/FoodMenuAdapter.java
+++ app/src/main/java/com/sparkdev/foodapp/mainscreen/FoodMenu/FoodMenuAdapter.java
@@ -8,27 +8,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.sparkdev.foodapp.R;
+
 import java.util.ArrayList;
 
 public class FoodMenuAdapter extends RecyclerView.Adapter<FoodMenuAdapter.ContactViewHolder>{
 
     private final ArrayList<String> menuItem;    // This will hold your data
     private final ArrayList<Integer>pictures;
+    private final ArrayList<String> itemPrice;
+    private final ArrayList<String> categories;
+    private final ArrayList<Double> ratings;
+    private final int clockIcon;
+    private final int starIcon;
+    private final int leafIcon;
+
+
     private LayoutInflater menuInflater;       // This will be the inflater for FoodMenuAdapter
 
+
     // FoodMenuAdapter Constructor
-    public FoodMenuAdapter(Context context, ArrayList<String> foodItem, ArrayList<Integer> pictures) {
+    public FoodMenuAdapter(Context context, ArrayList<String> foodItem, ArrayList<Integer> pictures,
+                           ArrayList<String> itemPrice,ArrayList<String> categories, ArrayList<Double> ratings,
+                           int clockIcon,int starIcon, int leafIcon)
+    {
         menuInflater = LayoutInflater.from(context); // Initialize the layout inflator
         this.menuItem = foodItem; // Initialize the arraylist
         this.pictures = pictures;
+        this.itemPrice = itemPrice;
+        this.categories = categories;
+        this.ratings = ratings;
+        this.clockIcon = clockIcon;
+        this.starIcon = starIcon;
+        this.leafIcon = leafIcon;
     }
 
     // Inner class to the FoodMenuAdapter and extends
     public class ContactViewHolder extends RecyclerView.ViewHolder{
         // The following variables are for the text view and the adapter for each row
         public final TextView nameTextView;
+        public final TextView itemPrice;
         public final ImageView foodImage;
+        public final TextView itemRate;
+        public final TextView itemCategory;
+        public final ImageView clockPic;
+        public final ImageView leafPic;
+        public final ImageView starPic;
         final FoodMenuAdapter rowAdapter;
 
         // Constructor where the first parameter is to inflate the layout and the second
@@ -38,8 +64,16 @@ public class FoodMenuAdapter extends RecyclerView.Adapter<FoodMenuAdapter.Contac
             // Initialize the view holder's text view from the XML resources (activity_contact_list.xml)
             // Be sure to cast it to the View type that you need it to be (i.e TextView)
             nameTextView = (TextView) itemView.findViewById(R.id.menuItem_name);
-            foodImage  = (ImageView) itemView.findViewById(R.id.foodItem_image) ;
-            // Set up the adapter
+            itemPrice = (TextView) itemView.findViewById(R.id.price_textView);
+            itemRate = (TextView) itemView.findViewById(R.id.rate_textView);
+            itemCategory = (TextView) itemView.findViewById(R.id.categories_textView);
+            foodImage  = (ImageView) itemView.findViewById(R.id.foodItem_image);
+            foodImage.setClipToOutline(true);
+            clockPic = (ImageView) itemView.findViewById(R.id.clock_icon);
+            leafPic = (ImageView) itemView.findViewById(R.id.leaf_icon);
+            starPic = (ImageView) itemView.findViewById(R.id.star_icon);
+
+             //Set up the adapter
             this.rowAdapter = adapter;
         }
     }
@@ -60,10 +94,16 @@ public class FoodMenuAdapter extends RecyclerView.Adapter<FoodMenuAdapter.Contac
     @Override
     public void onBindViewHolder(@NonNull ContactViewHolder contactViewHolder, int i) {
         String currentContact = menuItem.get(i);     // Hold the current contact name
+        String currentItemPrice = itemPrice.get(i);
+        String currentRate = Double.toString(ratings.get(i));
         int currentImage = pictures.get(i);
+        String currentCategory = categories.get(i);
 
         contactViewHolder.nameTextView.setText(currentContact); // Set contact name at i position to TextView
         contactViewHolder.foodImage.setImageResource(currentImage);
+        contactViewHolder.itemPrice.setText(currentItemPrice);
+        contactViewHolder.itemRate.setText(currentRate);
+        contactViewHolder.itemCategory.setText(currentCategory);
     }
 
     @Override
