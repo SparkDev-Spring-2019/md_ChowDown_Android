45a14649de87f42fd6e3de23bdbe255e70bb4963
msblaise <42875058+msblaise@users.noreply.github.com>
2019-03-31 00:53:38 -0400

    Completed validation for user input text fields

--- app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettings.java
+++ app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettings.java
@@ -3,43 +3,42 @@ package com.sparkdev.foodapp.profileSettings;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.util.AdapterListUpdateCallback;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Patterns;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.sparkdev.foodapp.R;
+import com.sparkdev.foodapp.models.User;
 
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /*
-This class represents the ProfileSettingsActivity which will display all the row titles from an ArrayList.
+This class represents the ProfileSettingsActivity which will display all the row titles from an ArrayList
+and validate user input
  */
 
-public class ProfileSettings extends AppCompatActivity {
-    private static final Pattern PASSWORD_PATTERN =
-            Pattern.compile("^" +
-                    //"(?=.*[0-9])" +         //at least 1 digit
-                    //"(?=.*[a-z])" +         //at least 1 lower case letter
-                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[a-zA-Z])" +      //any letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    "(?=\\S+$)" +           //no white spaces
-                    ".{6,}" +               //at least 6 characters
-                    "$");
-    private TextInputLayout textInputEmail;
-    private TextInputLayout textInputUsername;
-    private TextInputLayout textInputPassword;
+public class ProfileSettings extends AppCompatActivity   {
     private RecyclerView profileRecyclerView;
     private ProfileSettingsAdapter profileAdapter;
     private ArrayList <String> row_titles = new ArrayList<>();
+    private ArrayList <String> user_input = new ArrayList<>();
+    //private User u = new User();
     private Toolbar myToolbar;
+    private CircleImageView circleImageView;
+    private Menu menuItem;
+    private View view;
+    private EditText editText;
 
-    // The ContactListActivity enters the created state when the activity is created for the first
+    // The ProfileSettingsActivity enters the created state when the activity is created for the first
     // time (i.e. when the user opens the application).
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +65,15 @@ public class ProfileSettings extends AppCompatActivity {
         // Define the RecyclerView's default layout manager
         profileRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //this.profileAdapter = new ProfileSettingsAdapter(this);
+
+        myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
+        circleImageView = (CircleImageView) findViewById(R.id.profile_image);
+        circleImageView.setImageResource(R.mipmap.g1321);
+
+        menuItem = (Menu) findViewById(R.id.action_save);
     }
 
     @Override
@@ -77,50 +83,19 @@ public class ProfileSettings extends AppCompatActivity {
         return true;
     }
 
-
-    private boolean validateEmail() {
-        String emailInput = textInputEmail.getEditText().getText().toString().trim();
-
-        if(emailInput.isEmpty()) {
-            textInputEmail.setError("Field cannot be empty");
-            return false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            textInputEmail.setError("Please enter a valid email address");
-            return false;
-        } else {
-            textInputEmail.setError(null);
-            return true;
-        }
-    }
-
-    private boolean validateUsername() {
-        String usernameInput = textInputUsername.getEditText().getText().toString().trim();
-
-        if(usernameInput.isEmpty()) {
-            textInputUsername.setError("Field cannot be empty");
-            return false;
-        } else if (usernameInput.length() > 15){
-            textInputUsername.setError("Username too long");
-            return false;
-        } else {
-            textInputUsername.setError(null);
-            return true;
+    /*public void confirmInput(View v) {
+        if (!validateEmail() | !validateUsername() | !validatePassword()) {
+            return;
         }
 
-    }
+        String input = "Email: " + textInputEmail.getEditText().getText().toString();
+        input += "\n";
+        input += "Username: " + textInputUsername.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
 
-    private boolean validatePassword() {
-        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    }*/
 
-        if(passwordInput.isEmpty()) {
-            textInputPassword.setError("Field cannot be empty");
-            return false;
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            textInputPassword.setError("Password too weak");
-            return false;
-        } else {
-            textInputPassword.setError(null);
-            return true;
-        }
-    }
+
 }
--- app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettingsAdapter.java
+++ app/src/main/java/com/sparkdev/foodapp/profileSettings/ProfileSettingsAdapter.java
@@ -5,33 +5,49 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.util.AdapterListUpdateCallback;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
-
-public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettingsAdapter.ProfileViewHolder>{
+public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettingsAdapter.ProfileViewHolder> {
 
     private final ArrayList<String> textInputList;    // This will hold your data
     private LayoutInflater profileInflater;       // This will be the inflater for ContactListAdapter
 
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    ".{6,}" +               //at least 6 characters
+                    "$");
+
+
+
     // ContactListAdapter Constructor
-    public ProfileSettingsAdapter (Context context, ArrayList<String> textInputList) {
+    public ProfileSettingsAdapter(Context context, ArrayList<String> textInputList) {
         profileInflater = LayoutInflater.from(context); // Initialize the layout inflator
         this.textInputList = textInputList; // Initialize the arraylist
+        
     }
 
     // Inner class to the ContactListAdapter and extends
-    public class ProfileViewHolder extends RecyclerView.ViewHolder{
+    public class ProfileViewHolder extends RecyclerView.ViewHolder {
         // The following variables are for the text view and the adapter for each row
         public final TextView nameTextView;
         public final TextInputLayout editTextView;
         final ProfileSettingsAdapter rowAdapter;
+
 
         // Constructor where the first parameter is to inflate the layout and the second
         // parameter is to associate the ContactViewHolder with its adapter
@@ -44,6 +60,7 @@ public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettings
 
             // Set up the adapter
             this.rowAdapter = adapter;
+
         }
     }
 
@@ -62,14 +79,100 @@ public class ProfileSettingsAdapter extends RecyclerView.Adapter<ProfileSettings
 
     // The onBindViewHolder() connects your data to your view holder
     @Override
-    public void onBindViewHolder(@NonNull ProfileViewHolder contactViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final ProfileViewHolder contactViewHolder, final int i) {
+
         String currentInput = textInputList.get(i);     // Hold the current text field
         contactViewHolder.nameTextView.setText(currentInput); // Set text field at i position to TextView
-        contactViewHolder.editTextView.getEditText().setText("Guirdelle");
+        contactViewHolder.editTextView.getEditText().setText("Guirdelle"); //previous user input
+
+        contactViewHolder.editTextView.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                //validate each row
+                if (i == 0) {
+
+                    //first name
+                    String firstNameInput = contactViewHolder.editTextView.getEditText().getText().toString().trim();
+
+                    if (firstNameInput.isEmpty()) {
+                        contactViewHolder.editTextView.setError("Field cannot be empty");
+                    } else if (!firstNameInput.matches("[a-zA-Z]*")) {
+                        contactViewHolder.editTextView.setError("Field can only contain letters");
+                    } else {
+                        contactViewHolder.editTextView.setError(null);
+                    }
+
+                } else if (i == 1) {
+
+                    //last name
+                    String lastNameInput = contactViewHolder.editTextView.getEditText().getText().toString().trim();
+
+                    if (lastNameInput.isEmpty()) {
+                        contactViewHolder.editTextView.setError("Field cannot be empty");
+                    } else if (!lastNameInput.matches("[a-zA-Z]*")) {
+                        contactViewHolder.editTextView.setError("Field can only contain letters");
+                    } else {
+                        contactViewHolder.editTextView.setError(null);
+                    }
+
+                } else if (i == 2) {
+
+                    //username
+                    String usernameInput = contactViewHolder.editTextView.getEditText().getText().toString().trim();
+
+                    if (usernameInput.isEmpty()) {
+                        contactViewHolder.editTextView.setError("Field cannot be empty");
+                    } else if (usernameInput.length() > 15) {
+                        contactViewHolder.editTextView.setError("Username must be 15 characters or less");
+                    } else {
+                        contactViewHolder.editTextView.setError(null);
+                    }
+
+                } else if (i == 3) {
+
+                    //email
+                    String emailInput = contactViewHolder.editTextView.getEditText().getText().toString().trim();
+
+                    if(emailInput.isEmpty()) {
+                        contactViewHolder.editTextView.setError("Field cannot be empty");
+                    } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+                        contactViewHolder.editTextView.setError("Please enter a valid email address");
+                    } else {
+                        contactViewHolder.editTextView.setError(null);
+                    }
+
+                } else if (i == 4) {
+
+                    //password
+                    String passwordInput = contactViewHolder.editTextView.getEditText().getText().toString().trim();
+
+                    if(passwordInput.isEmpty()) {
+                        contactViewHolder.editTextView.setError("Password must be at least 6 characters");
+                    } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+                        contactViewHolder.editTextView.setError("Password too weak");
+                    } else {
+                        contactViewHolder.editTextView.setError(null);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
         return textInputList.size();
     }
+
 }
