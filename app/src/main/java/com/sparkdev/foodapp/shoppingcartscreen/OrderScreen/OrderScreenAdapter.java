232967b4a294f4ce492df5cf3244435e8498b21e
msblaise <42875058+msblaise@users.noreply.github.com>
2019-03-31 20:32:28 -0400

    Merge branch 'dev' of https://github.com/SparkDev-Spring-2019/md_FoodApp_Android into Settings_gb
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/sparkdev/foodapp/mainscreen/TabLayoutActivity.java
    #	app/src/main/java/com/sparkdev/foodapp/models/User.java
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/sparkdev/foodapp/shoppingcartscreen/OrderScreen/OrderScreenAdapter.java
+++ app/src/main/java/com/sparkdev/foodapp/shoppingcartscreen/OrderScreen/OrderScreenAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.bumptech.glide.Glide;
 import com.sparkdev.foodapp.R;
 import com.sparkdev.foodapp.models.OrderItem;
